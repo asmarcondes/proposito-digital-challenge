3b1849dc97b356e83d436ec8f098ec57de28fdea
Anderson Marcondes <anderson@marcond.es>
2023-02-02 11:55:11 +0000

    refactor: altera lógica if/else para hash

--- parser/log_parser.rb
+++ parser/log_parser.rb
@@ -17,10 +17,8 @@ class LogParser
     game_id = ''
     game_count = 0
 
-    File.foreach(get_file_path(filename)) do |line|
-      register = parse_line(line)
-
-      if register&.event == EVENT[:new_game]
+    events = {
+      EVENT[:new_game] => lambda { |_line|
         game_count += 1
         game_id = "#{GAME_PREFIX}#{game_count}"
 
@@ -29,23 +27,28 @@ class LogParser
           players: Set.new,
           kills: Hash.new(0)
         }
-      end
-
-      if register&.event == EVENT[:user_info]
-        player = register.content[LOG_PATTERNS[:player]]
+      },
+      EVENT[:user_info] => lambda { |line|
+        player = line.content[LOG_PATTERNS[:player]]
         games[game_id][:players].add(player)
         games[game_id][:kills][player] = 0 unless games[game_id][:kills].key?(player)
-      end
-
-      if register&.event == EVENT[:kill]
-        killer = register.content[LOG_PATTERNS[:killer]]
-        killed = register.content[LOG_PATTERNS[:killed]]
+      },
+      EVENT[:kill] => lambda { |line|
+        killer = line.content[LOG_PATTERNS[:killer]]
+        killed = line.content[LOG_PATTERNS[:killed]]
 
         games[game_id][:kills][killer] += 1 unless killer == WORLD_ID
         games[game_id][:kills][killed] -= 1 if killer == WORLD_ID
 
         games[game_id][:total_kills] += 1
-      end
+      }
+    }
+
+    File.foreach(get_file_path(filename)) do |line|
+      register = parse_line(line)
+
+      event_name = register&.event
+      events[event_name]&.call(register)
     end
 
     games
--- parser/modules/log.rb
+++ parser/modules/log.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+Line = Struct.new(:time, :event, :content)
+
 # Módulo com patterns/constantes referentes ao log
 module Log
   LOG_PATTERNS = {
