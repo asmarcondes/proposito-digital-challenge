7e0f0e5709e6ca370f610fb922ae376ce17a7f76
Anderson Marcondes <anderson@marcond.es>
2023-02-02 01:47:14 +0000

    feat: imprime relatório por jogo e ranking

--- parser/main.rb
+++ parser/main.rb
@@ -65,4 +65,40 @@ File.foreach(LOG_PATH + log_file) do |line|
   end
 end
 
-puts games
+ranking = Hash.new(0)
+
+games.each_with_index do |value, index|
+  puts "\n----- Game ##{index + 1} -----"
+  puts "\nPlayers:"
+
+  data = value.last
+  sorted_players = data[:players].sort
+
+  sorted_players.each do |player|
+    puts "- #{player}"
+  end
+
+  puts "\nKills:"
+  puts "- (#{data[:total_kills]})\tTOTAL KILLS"
+
+  sorted_kills = data[:kills].sort_by { |_a, b| -b }
+
+  sorted_kills.each do |kill|
+    ranking[kill.first] += kill.last
+    puts "- (#{kill.last})\t#{kill.first}"
+  end
+end
+
+puts "\n----- KILLS RANKING -----\n\n"
+
+current_position = 0
+last_kills = 0
+
+sorted_ranking = ranking.sort_by { |_a, b| -b }
+
+sorted_ranking.each_with_index do |kill, _index|
+  current_position += 1 unless kill.last == last_kills
+  last_kills = kill.last
+
+  puts "#{current_position}º #{kill.first} (#{kill.last})"
+end
